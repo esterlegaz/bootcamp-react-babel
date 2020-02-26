8eb95641204e9d2b8000133560562dc8df9e4007
Ester Legaz <esterlegaz@gmail.com>
2020-02-26 15:40:48 +0100

    feat(styles): fix styles responsive for product list

--- src/ProductsList/ProductsList.jsx
+++ src/ProductsList/ProductsList.jsx
@@ -9,24 +9,33 @@ class ProductsList extends React.Component {
             products: props.productList
         };
 
-    this.handleProductInteraction = this.handleProductInteraction.bind(this);
+        this.handleProductInteraction = this.handleProductInteraction.bind(this);
 
     }
     handleProductInteraction(product) {
         this.props.handleClick(product);
-        this.setState({products: this.props.productList});
+        this.setState({ products: this.props.productList });
     }
 
     render() {
-        const icons = (
-            <div className="product__icons">
-                <i className={"product__icon material-icons " + (!this.props.isGrid ? 'product__icon--selected' : '')} onClick={this.props.handleIsNotGrid}>view_list</i>
-                <i className={"product__icon material-icons " + (this.props.isGrid ? 'product__icon--selected' : '')} onClick={this.props.handleIsGrid}>view_module</i>
-            </div>
-        );
+        function checkMobileDevice() {
+            return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+        };
+        const isMobile = checkMobileDevice();
+
+        const renderIcons = () => {
+            if (!isMobile) {
+                return (
+                    <div className="product__icons">
+                        <i className={"product__icon material-icons " + (!this.props.isGrid ? 'product__icon--selected' : '')} onClick={this.props.handleIsNotGrid}>view_list</i>
+                        <i className={"product__icon material-icons " + (this.props.isGrid ? 'product__icon--selected' : '')} onClick={this.props.handleIsGrid}>view_module</i>
+                    </div>
+                )
+            }
+        };
         const product = this.state.products.map(product => {
             return (
-                <li key={product.id} className={`product__item ${!this.props.isGrid ? 'product__item--flex' : ''}`}>
+                <li key={product.id} className={`product__item ${!this.props.isGrid && !isMobile ? 'product__item--flex' : ''}`}>
                     <img src={product.image} alt={product.name} />
                     <div className={`${!this.props.isGrid ? 'product__info' : ''}`}>
                         <h2 className="product__title">{product.name}</h2>
@@ -34,14 +43,14 @@ class ProductsList extends React.Component {
                             <p>({product.createdAt.toLocaleDateString()}) - {product.description}</p>
                         )}
                         <p className="product__price">{product.price}€</p>
-                        <BtnProducts handleClick={() =>this.handleProductInteraction(product)} text={this.props.text} />
+                        <BtnProducts handleClick={() => this.handleProductInteraction(product)} text={this.props.text} />
                     </div>
                 </li>
             )
         })
         return (
             <div className="products__list">
-                {icons}
+                {renderIcons()}
                 <ul className={`${this.props.isGrid ? 'products__grid' : ''}`}>{product}</ul>
             </div>
         )
