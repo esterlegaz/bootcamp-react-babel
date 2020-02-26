f6e8b663ad5f6413a16cd4a2d466b0a10a30a6f2
Ester Legaz <esterlegaz@gmail.com>
2020-02-26 12:22:44 +0100

    chore(dist): generate dist

---
+++ docs/build/precache-manifest.76b6e439858f4a337734934a6ac45266.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceafad964a4041bc6270b9d000ae988d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4e40ab603a4ba48c097e",
+    "url": "./static/css/main.4aeea1ce.chunk.css"
+  },
+  {
+    "revision": "2c2e654599740c286ec2",
+    "url": "./static/js/2.8bea6bc7.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "./static/js/2.8bea6bc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e40ab603a4ba48c097e",
+    "url": "./static/js/main.04753dd4.chunk.js"
+  },
+  {
+    "revision": "b35b2bac434a1ace2537",
+    "url": "./static/js/runtime-main.037f70ee.js"
+  }
+]);
