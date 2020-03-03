ad7c072a4bc42d04bfd67f7ed96d30075df6dc2d
Ester Legaz <esterlegaz@gmail.com>
2020-03-03 09:03:55 +0100

    chore(dist): upload correct docs folder for build

---
+++ docs/precache-manifest.68d7f091f8a56b0343bec6616755a3a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbdd6c873e56f1b7719b65f9678410b3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "49801e74fa798bafbea0",
+    "url": "./static/css/main.41350b2e.chunk.css"
+  },
+  {
+    "revision": "dd7f4cebd2a7600939c8",
+    "url": "./static/js/2.2b687ba5.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "./static/js/2.2b687ba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49801e74fa798bafbea0",
+    "url": "./static/js/main.70ee2265.chunk.js"
+  },
+  {
+    "revision": "b35b2bac434a1ace2537",
+    "url": "./static/js/runtime-main.037f70ee.js"
+  }
+]);
